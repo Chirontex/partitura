52714e1fbb10aea526833aab2d4590716abfb3c2
Chironex <chirontex@yandex.com>
2022-05-01 04:41:46 +0300

    Убрать лишнее приведение типа.

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -123,7 +123,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier() : string
     {
-        return (string)$this->getUsername();
+        return $this->getUsername();
     }
 
     /**
