d61129b60da953970bb15774bff89fa7feb89d9f
Chironex <chirontex@yandex.com>
2022-05-05 12:24:40 +0300

    Подключить скрипты к шаблонам формы и логина. Вынести общие стили для админских форм из login.css в form.css.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -20,6 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
+    .addEntry('admin/form', './assets/genesis/admin/form/form.js')
     .addEntry('admin/login', './assets/genesis/admin/login/login.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
