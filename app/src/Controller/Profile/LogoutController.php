0cadee798e716688f853dfa85d9af3b492335a9e
Chironex <chirontex@yandex.com>
2022-05-08 00:51:13 +0300

    Реализовать маршрут для деавторизации.

---
+++ app/src/Controller/Profile/LogoutController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Partitura\Controller\Profile;
+
+use Partitura\Exception\SystemException;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class LogoutController
+ * @package Partitura\Controller\Profile
+ * 
+ * @Route("/profile/logout")
+ */
+class LogoutController
+{
+    public const ROUTE_LOGOUT = "partitura_profile_logout";
+
+    /**
+     * @Route("/", name=LogoutController::ROUTE_LOGOUT, methods={"GET"})
+     */
+    public function logout() : void
+    {
+        throw new SystemException("Something is wrong with logout.");
+    }
+}
