573d20b3e73e58710f1672848cb509f9d908a4e8
Chironex <chirontex@yandex.com>
2022-06-04 20:48:55 +0300

    Добавить локатор для фабрик RequestDto.

---
+++ app/src/Locator/RequestDtoFactoryLocator.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Partitura\Locator;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Partitura\Interfaces\RequestDtoFactoryInterface;
+
+/**
+ * Class RequestDtoFactoryLocator
+ * @package Partitura\Locator
+ */
+class RequestDtoFactoryLocator
+{
+    /** @var ArrayCollection<RequestDtoFactoryInterface> */
+    protected $factories;
+
+    /**
+     * @param string $dtoClassName
+     * @param RequestDtoFactoryInterface $dtoFactory
+     *
+     * @return $this
+     */
+    public function addFactory(string $dtoClassName, RequestDtoFactoryInterface $dtoFactory) : static
+    {
+        $this->factories->set($dtoClassName, $dtoFactory);
+
+        return $this;
+    }
+
+    /**
+     * @param string $dtoClassName
+     *
+     * @return null|RequestDtoFactoryInterface
+     */
+    public function getFactory(string $dtoClassName) : ?RequestDtoFactoryInterface
+    {
+        return $this->factories->get($dtoClassName);
+    }
+}
