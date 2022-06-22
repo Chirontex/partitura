3b3d94cb219acba3fcb9f9e979776ed26e0a074a
Chironex <chirontex@yandex.com>
2022-06-23 02:02:47 +0300

    Применить FadeIn к элементам класса blog-post.

--- app/assets/genesis/main/blog.js
+++ app/assets/genesis/main/blog.js
@@ -1,1 +1,12 @@
 import './blog.css'
+
+const FadeIn = require('../main/src/Effects/FadeIn');
+
+(() => {
+    const posts = document.getElementsByClassName("blog-post");
+
+    for (let i = 0; i < posts.length; i++)
+    {
+        (new FadeIn(posts[i]));
+    }
+})()
