1f0be2612c82756480cf1a7414f761415bd288d7
Chironex <chirontex@yandex.com>
2022-06-23 03:09:53 +0300

    Добавить фейд для пагинаций блога.

--- app/assets/genesis/main/blog.js
+++ app/assets/genesis/main/blog.js
@@ -4,9 +4,17 @@ const FadeIn = require('../main/src/Effects/FadeIn');
 
 (() => {
     const posts = document.getElementsByClassName("blog-post");
+    const paginations = document.getElementsByClassName("blog-pagination");
+    let lastFade;
 
     for (let i = 0; i < posts.length; i++)
     {
-        (new FadeIn(posts[i], 500 * i));
+        lastFade = 500 * i;
+        (new FadeIn(posts[i], lastFade));
+    }
+
+    for (let i = 0; i < paginations.length; i++)
+    {
+        (new FadeIn(paginations[i], lastFade));
     }
 })()
