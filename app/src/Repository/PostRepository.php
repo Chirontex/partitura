f624abecb0af15356f481b71c3598840ebf02aff
Chironex <chirontex@yandex.com>
2022-06-25 00:43:35 +0300

    Revert "Добавить метод для подсчёта постов блога в репозиторий постов."
    This reverts commit 658c4fe938a7d2d0514c0a63e81cd0231f9ac21e.

--- app/src/Repository/PostRepository.php
+++ app/src/Repository/PostRepository.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Partitura\Repository;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\DBAL\Exception;
 use Doctrine\Persistence\ManagerRegistry;
 use Partitura\Entity\Post;
 
@@ -23,36 +22,5 @@ class PostRepository extends Repository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Post::class);
-    }
-
-    /**
-     * @throws Exception
-     * @return int
-     */
-    public function countBlogPosts() : int
-    {
-        return $this->countPosts("IN_BLOG = :in_blog", ["in_blog" => 1]);
-    }
-
-    /**
-     * @param string $where
-     * @param array $criteria
-     *
-     * @throws Exception
-     * @return int
-     */
-    protected function countPosts(string $where, array $criteria) : int
-    {
-        $sql = sprintf("SELECT COUNT(*) as \"count\" FROM %s t", Post::TABLE_NAME);
-
-        if (!empty($where)) {
-            $sql .= sprintf(" WHERE %s", $where);
-        }
-
-        $connection = $this->getEntityManager()->getConnection();
-        $statement = $connection->prepare($sql);
-        $result = $statement->executeQuery($criteria)->fetchAllAssociative();
-
-        return (int)$result["count"];
     }
 }
