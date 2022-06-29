e2c0163a257e976eb38f56d0573f5889a06eeebd
Chironex <chirontex@yandex.com>
2022-06-29 20:25:53 +0300

    Добавить BlogClient. Добавить тестовую загрузку постов в консоль браузера.

--- app/assets/genesis/main/blog.js
+++ app/assets/genesis/main/blog.js
@@ -1,8 +1,20 @@
 import './blog.css'
 
+const BlogClient = require('./src/Client/BlogClient');
 const FadeIn = require('./src/Effect/FadeIn');
 
+const requestPosts = (page = undefined, limit = undefined) => {
+    // TODO: добавить обработчик и заменить им консоль
+    (new BlogClient(console.log))
+        .setPage(page)
+        .setLimit(limit)
+        .requestPosts();
+};
+
+requestPosts();
+
 (() => {
+    // TODO: Убрать после добавления обработчика
     const posts = document.getElementsByClassName("blog-post");
     const paginations = document.getElementsByClassName("blog-pagination");
     let lastDelay;
---
+++ app/assets/genesis/main/src/Client/BlogClient.js
@@ -0,0 +1,96 @@
+'use strict';
+
+/**
+ * Class BlogClient
+ * @package Partitura.Genesis.Main.Client
+ */
+class BlogClient
+{
+    #blogApiUri = "/api/blog/";
+    #page;
+    #limit;
+
+    /** @var {callback} */
+    #handler;
+
+    constructor(handler)
+    {
+        this.#handler = handler;
+    }
+
+    /**
+     * @returns {undefined|int}
+     */
+    getPage = () => {
+        return this.#page;
+    }
+
+    /**
+     * @param {undefined|int} page
+     * @returns {this}
+     */
+    setPage = (page) => {
+        this.#page = page;
+
+        return this;
+    }
+
+    /**
+     * @returns {undefined|int}
+     */
+    getLimit = () => {
+        return this.#limit;
+    }
+
+    /**
+     * @param {undefined|int} limit
+     * @returns 
+     */
+    setLimit = (limit) => {
+        this.#limit = limit;
+
+        return this;
+    }
+
+    requestPosts = async () => {
+        const args = {page: this.#page, limit: this.#limit};
+        const argsKeys = Object.keys(args);
+        let uri = this.#blogApiUri;
+        let concatArgToUri = (uri, argKey, argValue) => {
+            uri += uri == this.#blogApiUri ? "?" : "&";
+            uri += argKey+"="+argValue;
+
+            return uri;
+        };
+        let argKey;
+        let argValue;
+
+        for (let i = 0; i < argsKeys.length; i++)
+        {
+            argKey = argsKeys[i];
+            argValue = args[argKey];
+
+            if (argValue == undefined)
+            {
+                continue;
+            }
+
+            uri = concatArgToUri(uri, argKey, argValue);
+        }
+
+        await fetch(uri)
+            .then(
+                (response) => {
+                    return response.json();
+                },
+                () => {
+                    return {posts: []};
+                }
+            )
+            .then((data) => {
+                this.#handler(data);
+            });
+    }
+}
+
+module.exports = BlogClient;
