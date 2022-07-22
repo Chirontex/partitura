1449a76073703b0cb0da15ea2fc33b1eb92349b6
Chironex <chirontex@yandex.com>
2022-07-22 08:13:24 +0300

    Добавить ограничение получения постов по методу.

--- app/src/Controller/PostController.php
+++ app/src/Controller/PostController.php
@@ -8,6 +8,7 @@ use Partitura\Manager\PostManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -27,12 +28,20 @@ class PostController extends AbstractController
     /**
      * @param Request $request
      * 
+     * @throws MethodNotAllowedHttpException
      * @throws NotFoundHttpException
      *
      * @return Response
      */
     public function showPost(Request $request) : Response
     {
+        if ($request->getMethod() !== Request::METHOD_GET) {
+            throw new MethodNotAllowedHttpException(
+                [Request::METHOD_GET],
+                sprintf("Posts are available only with %s method.", Request::METHOD_GET)
+            );
+        }
+
         try {
             // TODO: Убрать это в фабрику DTO после добавления шаблона для постов.
             $post = $this->postManager->getPostByUri($request->getPathInfo());
