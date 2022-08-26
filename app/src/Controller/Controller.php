ea1fb8ef6242e7ed0eaabdcb6906f61f9b427bda
Chironex <chirontex@yandex.com>
2022-08-26 14:08:07 +0300

    Добавить методы получения данных пользователя и определения необходимости включения юзерпанели.

--- app/src/Controller/Controller.php
+++ app/src/Controller/Controller.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Partitura\Controller;
 
+use Partitura\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -18,5 +19,27 @@ abstract class Controller extends AbstractController
     {
         // TODO: Реализовать получение имени сайта из настроек, когда настройки будут реализованы.
         return "Partitura";
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isUserPanelAvailable() : bool
+    {
+        // TODO: Реализовать получение этой настройки из БД.
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    protected function getUserData() : array
+    {
+        /** @var null|User */
+        $user = $this->getUser();
+
+        return $user === null ? [] : [
+            "user_identifier" => $user->getUserIdentifier(),
+        ];
     }
 }
