bd028eb5e737e97e2b128491bd16e4aff9ac53c3
Chironex <chirontex@yandex.com>
2022-10-29 00:03:03 +0300

    Перенести класс стилей для кнопки сабмита в основной файл стилей профиля.

--- app/assets/genesis/profile/main_info.js
+++ app/assets/genesis/profile/main_info.js
@@ -1,5 +1,3 @@
-import './main_info.css'
-
 const OnSubmitFormButtonSpinner = require('../main/src/Effect/OnSubmitFormButtonSpinner');
 
 (new OnSubmitFormButtonSpinner("main_info_form", "main_info_submit_button"));
