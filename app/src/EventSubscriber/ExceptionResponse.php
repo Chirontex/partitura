505412ec5865e6bc2a8054fcf87bc95499dab0f4
Chironex <chirontex@yandex.com>
2022-12-21 02:23:00 +0300

    Рефакторинг Partitura\EventSubscriber\ExceptionResponse. Добавить вывод ошибок валидации в шаблон представления.

--- app/src/EventSubscriber/ExceptionResponse.php
+++ app/src/EventSubscriber/ExceptionResponse.php
@@ -6,10 +6,13 @@ namespace Partitura\EventSubscriber;
 use InvalidArgumentException;
 use Partitura\EventSubscriber\Trait\RequestEventSubscriberTrait;
 use Partitura\Exception\ArgumentException;
+use Partitura\Interfaces\ViewResolverInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Throwable;
+use Twig\Environment;
 
 /**
  * Class ExceptionResponse
@@ -18,6 +21,18 @@ use Throwable;
 class ExceptionResponse implements EventSubscriberInterface
 {
     use RequestEventSubscriberTrait;
+
+    protected ViewResolverInterface $viewResolver;
+
+    protected Environment $twig;
+
+    public function __construct(
+        ViewResolverInterface $viewResolver,
+        Environment $twig
+    ) {
+        $this->viewResolver = $viewResolver;
+        $this->twig = $twig;
+    }
 
     /** {@inheritDoc} */
     public static function getSubscribedEvents() : array
@@ -31,33 +46,44 @@ class ExceptionResponse implements EventSubscriberInterface
     public function handleException(ExceptionEvent $event) : void
     {
         $e = $event->getThrowable();
+        $request = $event->getRequest();
+        $isNeedJsonResponse = $this->isNeedJsonResponse($request);
+        $responseCode = $this->getResponseCode($e, $isNeedJsonResponse);
 
-        if ($this->isNeedJsonResponse($event->getRequest())) {
+        if ($isNeedJsonResponse) {
             $event->setResponse(new JsonResponse(
                 ["message" => $e->getMessage()],
-                $this->getResponseCode($e)
+                $responseCode
             ));
+        } elseif ($e instanceof ArgumentException) {
+            // TODO: добавить получение параметров из контроллеров для проброса в рендер
+            $event->setResponse(new Response($this->twig->render(
+                $this->viewResolver->resolveViewByRoute(
+                    $request->attributes->get("_route")
+                ),
+                []
+            )));
         }
     }
 
     /**
      * @param Throwable $e
+     * @param bool isJsonResponse
      *
      * @return int
      */
-    protected function getResponseCode(Throwable $e) : int
+    protected function getResponseCode(Throwable $e, bool $isJsonResponse = false) : int
     {
         if ($e->getCode() > 399) {
             return $e->getCode();
         }
 
-        switch (get_class($e)) {
-            case ArgumentException::class:
-            case InvalidArgumentException::class:
-                return 400;
-
-            default:
-                return 500;
-        }
+        return match (get_class($e)) {
+            default => Response::HTTP_INTERNAL_SERVER_ERROR,
+            ArgumentException::class => $isJsonResponse
+                ? Response::HTTP_BAD_REQUEST
+                : Response::HTTP_OK,
+            InvalidArgumentException::class => Response::HTTP_BAD_REQUEST,
+        };
     }
 }
