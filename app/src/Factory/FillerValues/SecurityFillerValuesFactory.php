f3a0580667e28cba0c85c32e3d6bdfa8b43c92cd
Chironex <chirontex@yandex.com>
2023-02-23 17:46:48 +0300

    Добавить фабрику филлерных значений для страницы безопасности.

---
+++ app/src/Factory/FillerValues/SecurityFillerValuesFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Partitura\Factory\FillerValues;
+
+class SecurityFillerValuesFactory extends ProfileFillerValuesFactory
+{
+    /** {@inheritDoc} */
+    public static function getView() : string
+    {
+        return "genesis/profile/security.html.twig";
+    }
+}
